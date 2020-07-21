e94a6ad7ae7dd8e0156c1df4c41b558d0fda7163
Lalo <Lalo@EDUARDO>
2020-07-21 14:25:22 -0500

    Add project files.

---
+++ pso-binario/Particula.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pso_binario
+{
+    class Particula
+    {
+        private int[] valores;
+        private Random rand;
+
+        public int[] Valores
+        {
+            get { return valores; }
+            set { valores = value; }
+        }
+
+        public Particula()
+        {
+            valores = new int[8];
+            rand = new Random((int)DateTime.Now.Ticks);
+        }
+    }
+}
